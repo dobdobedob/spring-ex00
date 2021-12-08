64a0bebc35e318d1a38b53be7ab23dbc03126a98
dobdobedob <smkdkd1993@gmail.com>
2021-12-09 01:37:26 +0900

    몰?루?

--- src/main/java/org/zerock/mapper/p05mapper/Mapper01.java
+++ src/main/java/org/zerock/mapper/p05mapper/Mapper01.java
@@ -2,4 +2,8 @@ package org.zerock.mapper.p05mapper;
 
 public class Mapper01 {
 
+	@RequestMapping("/met03")
+	public void method03(String category) {
+		List<String> productNames = mapper.getProduct
+	}
 }
